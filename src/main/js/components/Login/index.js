e30c77cef83fbc0b54ccea77790dfdbacf56b962
edgarshmavonyan <shmavonyan.eha@phystech.edu>
2020-01-07 00:24:21 +0300

    add notification, registration working with notification and error show

--- src/main/js/components/Login/index.js
+++ src/main/js/components/Login/index.js
@@ -26,7 +26,7 @@ class Login extends React.Component {
     render() {
         return (
             <div>
-                <Form {...props} error={params.get('error')} />
+                <Form {...props} />
                 <h4><Link to={"/registration"}>Зарегистрироваться</Link></h4>
             </div>
     );
