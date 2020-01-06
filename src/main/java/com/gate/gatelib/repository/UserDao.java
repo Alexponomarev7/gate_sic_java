83e1a30068e6211149253db7a26a57108f9f9e0d
edgarshmavonyan <shmavonyan.eha@phystech.edu>
2020-01-06 03:33:33 +0300

    add registration basic

--- src/main/java/com/gate/gatelib/repository/UserDao.java
+++ src/main/java/com/gate/gatelib/repository/UserDao.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDao extends JpaRepository<User, Integer> {
     User findByUsername(String username);
+    boolean existsByUsername(String username);
 }
