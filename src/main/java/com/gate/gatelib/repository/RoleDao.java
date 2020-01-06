9120a53596a58c22231d53881b36adc98666ba4f
Alexponomarev7 <Alex.ponomarev7@ya.ru>
2020-01-06 22:56:14 +0300

    add jwt auth

--- src/main/java/com/gate/gatelib/repository/RoleDao.java
+++ src/main/java/com/gate/gatelib/repository/RoleDao.java
@@ -1,7 +1,11 @@
 package com.gate.gatelib.repository;
 
 import com.gate.gatelib.models.Role;
+import com.gate.gatelib.models.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleDao extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
 }
